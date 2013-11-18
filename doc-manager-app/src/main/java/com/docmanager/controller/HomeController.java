3c5c9f69ac6c34864a2ce74c21c37a56744e1b50
ptorrao <pedro.luis.torrao@gmail.com>
2013-11-18 16:54:35 +0100

    Login page layout changes.

--- doc-manager-app/src/main/java/com/docmanager/controller/HomeController.java
+++ doc-manager-app/src/main/java/com/docmanager/controller/HomeController.java
@@ -1,16 +1,9 @@
 package com.docmanager.controller;
 
-import com.docmanager.TestForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -22,23 +15,4 @@ public class HomeController {
         return "home/index";
     }
 
-    @ModelAttribute("formDTO")
-    public TestForm createFormBean() {
-        return new TestForm();
-    }
-
-    @RequestMapping(value = "/", method=RequestMethod.POST)
-    public String submitMessage(@Valid TestForm testForm, BindingResult result,
-                                SessionStatus sessionStatus,
-                                RedirectAttributes redirectAttrs) {
-
-        if (result.hasErrors()) {
-            return "home";
-        }
-        String message = testForm.getMessageFromUser();
-        sessionStatus.setComplete();
-        redirectAttrs.addFlashAttribute("message", message);
-
-        return "redirect:/";
-    }
 }
