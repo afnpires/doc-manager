30f58bbfb2678c79a30f204e23a346afb95fbac6
Pedro Diogo Serra <pdiogomserra@gmail.com>
2013-11-18 19:00:24 +0100

    Fixed HomeController code

--- doc-manager-app/src/main/java/com/docmanager/aspects/LoggerAspect.java
+++ doc-manager-app/src/main/java/com/docmanager/aspects/LoggerAspect.java
@@ -9,6 +9,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,7 +23,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggerAspect {
 
-    private final Log log = LogFactory.getLog(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Before("execution(public * com.docmanager.controller..*.*(..)")
     public void logMethodCall(JoinPoint joinPoint) throws Throwable{
--- doc-manager-app/src/main/java/com/docmanager/controller/HomeController.java
+++ doc-manager-app/src/main/java/com/docmanager/controller/HomeController.java
@@ -1,10 +1,5 @@
 package com.docmanager.controller;
 
-<<<<<<< HEAD
-
-import com.docmanager.TestForm;
-=======
->>>>>>> 6957899c56a8d3b3e51847ecc6f9364323990147
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,28 +14,4 @@ public class HomeController {
         model.addAttribute("message", "Hello world!");
         return "home/index";
     }
-
-<<<<<<< HEAD
-    @ModelAttribute("formDTO")
-    public TestForm createFormBean() {
-        return new TestForm();
-    }
-
-    @RequestMapping(value = "/", method=RequestMethod.POST)
-    public String submitMessage(@Valid TestForm testForm, BindingResult result,
-                                SessionStatus sessionStatus,
-                                RedirectAttributes redirectAttrs) {
-
-        if (result.hasErrors()) {
-            return "home";
-        }
-        String message = testForm.getMessageFromUser();
-        sessionStatus.setComplete();
-        redirectAttrs.addFlashAttribute("message", message);
-
-        return "redirect:/";
-    }
-
-=======
->>>>>>> 6957899c56a8d3b3e51847ecc6f9364323990147
 }
